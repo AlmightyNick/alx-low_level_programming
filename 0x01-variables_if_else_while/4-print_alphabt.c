3ce45335b96a1c5cbf3804cf35008cd0e8183065
AlmightyNick <Nick.mbugua15@gmail.com>
2022-09-22 03:41:10 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-	char low, e;
+	char low, e, q;
 
 	e = 'e' ;
 	q = 'q' ;
 
 	for (low = 'a' ; low <= 'z' ; low++)
 	{
-		if (low != e && low ('!') = q)
+		if (low != e && low '!' = q)
 			putchar(low);
 	}
 	printf("\n");
